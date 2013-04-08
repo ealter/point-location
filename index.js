42a93e49e8a033fa09b447455e55d3241097943e
Eliot Alter <Eliot.Alter@gmail.com>
2013-04-07 23:43:38 -0400

    Switched from using d3 to using jquery + jcanvas

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-var width = 700;
-var height = 500;
 var currentPolygon = []; //An array of points
 var allPolygons = [currentPolygon];    //An array of point arrays. Includes currentPolygon
 
-var svg = d3.select("body").append("svg")
-                           .attr("width", width)
-                           .attr("height", height)
-                           .style("border", "1px solid black");
+var canvas = $("#canvas");
+var canvasPosition = {
+    x: canvas.offset().left,
+    y: canvas.offset().top
+};
 
-var path = svg.append('svg:g').selectAll('path'),
-    circle = svg.append('svg:g').selectAll('g');
-
-svg.on("click", function() {
-  var p = snapToPoint(new Point(d3.event.offsetX, d3.event.offsetY), currentPolygon);
+canvas.on("click", function(e) {
+  var mouse = new Point(e.pageX - canvasPosition.x, e.pageY - canvasPosition.y);
+  var p = snapToPoint(mouse, currentPolygon);
   console.log(p);
   if(canAppendPointToPolygon(currentPolygon, p)) {
     currentPolygon.push(p);
     var isFinishingPolygon = currentPolygon.length >= 3 && p.equals(currentPolygon[0]);
-    redrawPolygon(currentPolygon, isFinishingPolygon);
     if(isFinishingPolygon) {
-      //drawTriangulation(currentPolygon);
+      console.log("Finishing polygon");
       currentPolygon.pop();
-      triangulateEarClipping(currentPolygon);
+      //triangulateEarClipping(currentPolygon);
       currentPolygon = [];
       allPolygons.push(currentPolygon);
     }
+    render();
   } else {
     console.log("self intersecting");
     drawTemporarySegment(p, currentPolygon[currentPolygon.length - 1]);
   }
 });
-
-function triangulate(polygon) {
-  return d3.geom.delaunay(currentPolygon.map(function (vertex) {
-    return [vertex.x, vertex.y];
-  }));
-}
-
-function drawTriangulation(polygon) {
-  path.data(triangulate(polygon))
-      .enter().append("path")
-      .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-      .style("fill", "None")
-      .style("stroke", "green");
-}
 
 function snapToPoint(p, polygon) {
   //If (x,y) is close to a point on the polygon, it returns that point
@@ -57,16 +40,7 @@ function snapToPoint(p, polygon) {
 }
 
 function drawTemporarySegment(p1, p2) {
-  var line = svg.append("svg:line")
-                .attr("x1", p1.x)
-                .attr("y1", p1.y)
-                .attr("x2", p2.x)
-                .attr("y2", p2.y)
-                .style("stroke", "red")
-                .transition()
-                .duration(600)
-                .style("opacity", 0)
-                .remove();
+  console.log("Temporary segment TODO");
 }
 
 function segmentIntersectsAnyPolygon(seg) {
@@ -82,29 +56,30 @@ function canAppendPointToPolygon(polygon, p) {
      !segmentIntersectsAnyPolygon(new LineSegment(polygon[polygon.length - 1], p)));
 }
 
-function redrawPolygon(polygon, highlight) {
-  var g = circle.data(polygon).enter();
-  g.append('circle')
-    .attr('class', 'vertex')
-    .attr('r', 5)
-    .attr('cx', function(d) { return d.x; })
-    .attr('cy', function(d) { return d.y; });
-  var line = d3.svg.line()
-    .x(function(d) { return d.x; })
-    .y(function(d) { return d.y; });
-  var lineData = g.append("svg:path")
-    .attr("d", line(polygon))
-    .style("fill", "None")
-    .style("stroke-width", 3);
-  if(highlight) {
-    lineData.style("stroke", "blue")
-            .transition()
-            .duration(800)
-            .style("stroke", "black")
-            .style("stroke-width", 1);
-  } else {
-    lineData.style("stroke", "black")
-            .style("stroke-width", 1);
-  }
+function render() {
+  canvas.clearCanvas();
+    console.log(allPolygons);
+  allPolygons.forEach(function (polygon) {
+    for(var i=0; i<polygon.length; i++) {
+      canvas.drawArc({
+        fillStyle: "black",
+        strokeStyle: "black",
+        x: polygon[i].x,
+        y: polygon[i].y,
+        radius: 5
+      });
+    }
+    //Draw the lines connecting them
+    var line = {
+      strokeStyle: "black",
+      strokeWidth: 1,
+      closed: polygon !== currentPolygon
+    };
+    for(var i=0; i<polygon.length; i++) {
+      line['x' + (i+1)] = polygon[i].x;
+      line['y' + (i+1)] = polygon[i].y;
+    }
+    canvas.drawLine(line);
+  });
 }
 
